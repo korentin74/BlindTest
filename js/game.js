178ad19dee398348682491de9e39307469247a4f
korentin74 <128639790+korentin74@users.noreply.github.com>
2026-02-02 17:15:56 +0100

    Ajout playlist et score

--- js/game.js
+++ js/game.js
@@ -1,20 +1,52 @@
 const audio = document.getElementById("audio");
 const result = document.getElementById("result");
 
-const goodAnswer = "joe le taxi";
+let score = 0;
+let current = 0;
 
-// extrait test
-audio.src = "assets/audio/joe_le_taxi.mp3";
-audio.load();
+const musiques = [
+  {
+    src: "assets/audio/joe_le_taxi.mp3",
+    answer: "joe le taxi"
+  },
+  {
+    src: "assets/audio/test.mp3",
+    answer: "test"
+  }
+];
+
+// charger une musique
+function loadMusic() {
+  audio.src = musiques[current].src;
+  audio.load();
+  result.textContent = "";
+  document.getElementById("answer").value = "";
+}
+
+loadMusic();
 
 function checkAnswer() {
-  const userAnswer = document.getElementById("answer").value.toLowerCase();
+  const input = document.getElementById("answer").value.toLowerCase();
 
-  if (userAnswer.includes(goodAnswer)) {
+  if (input.includes(musiques[current].answer)) {
     result.textContent = "✅ Bonne réponse !";
     result.style.color = "#00ffcc";
+    score++;
   } else {
     result.textContent = "❌ Mauvaise réponse";
     result.style.color = "red";
+  }
+
+  setTimeout(nextMusic, 1200);
+}
+
+function nextMusic() {
+  current++;
+
+  if (current < musiques.length) {
+    loadMusic();
+  } else {
+    result.textContent = `🎉 Partie terminée ! Score : ${score}/${musiques.length}`;
+    audio.pause();
   }
 }
