7d34e3588dffa62884cc78e1f8356153eda22faf
korentin74 <128639790+korentin74@users.noreply.github.com>
2026-02-02 16:57:11 +0100

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ const result = document.getElementById("result");
 const goodAnswer = "despacito";
 
 // extrait test (à remplacer plus tard)
-audio.src = "assets/audio/Vanessa Paradis - Joe le taxi.mp3";
+audio.src = "assets/audio/joe_le_taxi.mp3";
 
 function checkAnswer() {
   const userAnswer = document.getElementById("answer").value.toLowerCase();
