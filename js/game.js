987f138b591c7b577d3d4d4bef2a2f0723d6c623
korentin74 <128639790+korentin74@users.noreply.github.com>
2026-02-02 16:43:10 +0100

    Ajout logique du jeu

---
+++ js/game.js
@@ -0,0 +1,19 @@
+const audio = document.getElementById("audio");
+const result = document.getElementById("result");
+
+const goodAnswer = "despacito";
+
+// extrait test (à remplacer plus tard)
+audio.src = "assets/audio/test.mp3";
+
+function checkAnswer() {
+  const userAnswer = document.getElementById("answer").value.toLowerCase();
+
+  if (userAnswer.includes(goodAnswer)) {
+    result.textContent = "✅ Bonne réponse !";
+    result.style.color = "#00ffcc";
+  } else {
+    result.textContent = "❌ Mauvaise réponse";
+    result.style.color = "red";
+  }
+}
